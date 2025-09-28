5fc6ac87fde6c94dcb0ba3003719e035b8b3685e
Shiyas ps <shiyasofficial1@gmail.com>
2025-09-28 14:35:47 +0530

    Testing the DB

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 btn = document.getElementById("account")
 
-btn.addEventListner("click",function(){
-
+btn.addEventListner("click",function(){ 
     fetch('http://localhost:5000/get-users')
     .then(response => response.json())
     .then(data => alert('Backend says:', data))
