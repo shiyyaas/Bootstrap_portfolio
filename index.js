7e5ffd1d86ad22e367d9c4427a3e9c9c330d383a
Shiyas ps <shiyasofficial1@gmail.com>
2025-09-28 12:37:46 +0530

    Testing the DB

---
+++ index.js
@@ -0,0 +1,14 @@
+btn = document.getElementById("account")
+
+btn.addEventListner("click",function(){
+
+    fetch('http://localhost:5000/get-users')
+    .then(response => response.json())
+    .then(data => alert('Backend says:', data))
+    .catch(error => {
+        alert("ERROR : " + error)
+    });
+})
+    
+    
+    
