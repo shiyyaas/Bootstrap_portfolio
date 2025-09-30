40abd41727fe33cf5fa626202e258176417f67ae
Shiyas ps <shiyasofficial1@gmail.com>
2025-09-30 10:58:35 +0530

    Added the show functionality for registration form

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-btn = document.getElementById("account")
+const btn = document.getElementById("account")
+const form = document.getElementById("Register")
+// btn.addEventListener("click",function(){ 
+//     fetch('http://localhost:5000/get-users')
+//     .then(response => response.json())
+//     .then(data => {
+//         let message = `Found ${data.length} users:\n`;
+//         data.forEach(data => {
+//             message += `NAME : ${data.name}, EMAIL: ${data.email}\n`;
+//         });
+//         alert(message);
+//     })
+//     .catch(error => {
+//         alert("ERROR : " + error)
+//     });
+// })
 
-btn.addEventListener("click",function(){ 
-    fetch('http://localhost:5000/get-users')
-    .then(response => response.json())
-    .then(data => {
-        let message = `Found ${data.length} users:\n`;
-        data.forEach(data => {
-            message += `NAME : ${data.name}, EMAIL: ${data.email}\n`;
-        });
-        alert(message);
-    })
-    .catch(error => {
-        alert("ERROR : " + error)
-    });
-})
+btn.addEventListener("click", function(){
+    form.classList.remove('d-none');
+    form.classList.add('d-block');
+});
