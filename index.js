54d5d76e0cb8bafaf36ebf712ccc79c9d8b8a9a5
Shiyas ps <shiyasofficial1@gmail.com>
2025-10-01 19:09:57 +0530

    Backend data is ready

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const btn = document.getElementById("account")
+const formdisplay = document.getElementById("formdisplay")
 const form = document.getElementById("Register")
 
-// btn.addEventListener("click",function(){ 
-//     fetch('http://localhost:5000/get-users')
-//     .then(response => response.json())
-//     .then(data => {
-//         let message = `Found ${data.length} users:\n`;
-//         data.forEach(data => {
-//             message += `NAME : ${data.name}, EMAIL: ${data.email}\n`;
-//         });
-//         alert(message);
-//     })
-//     .catch(error => {
-//         alert("ERROR : " + error)
-//     });
-// })
-
 btn.addEventListener("click", function(){
-    form.classList.remove('d-none');
-    form.classList.add('d-block');
+    formdisplay.classList.remove('d-none');
+    formdisplay.classList.add('d-block');
 });
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let name = form.elements.name.value;
+    let email = form.elements.email.value;
+    let password = form.elements.password.value;
+
+    const userdata = {
+        "name" : name,
+        "email" : email,
+        "password": password
+    };
+})
